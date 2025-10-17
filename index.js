bcb5dc3904079672166eb46348f981b8af16ff58
olatunji0001 <musbautunji@gmail>
2025-10-17 19:27:40 +0100

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import fetch from "node-fetch";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 45687 || 5000;
 
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
